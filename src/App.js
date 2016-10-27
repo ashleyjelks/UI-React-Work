a7f9b034297199ab1129db3d1f7a7aee39b739e6
Imogen Wentworth <imogen.wentworth@gmail.com>
2016-10-27 16:29:49 -0700

    Add mock candidate data

--- src/App.js
+++ src/App.js
@@ -3,12 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  fetchCandidates() {
+    fetch('/candidates.json')
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(candidates) {
+        console.log(candidates);
+      });
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
