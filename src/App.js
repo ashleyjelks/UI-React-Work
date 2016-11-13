5ce68594df99e39144d4249562d798d7c42cc677
Ashley Jelks <arjelks@gmail.com>
2016-11-12 19:24:13 -0800

    work in progress adding styles

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import Candidates from './Candidates';
 import logo from './logo.svg';
 import './App.css';
+import AutoComplete from 'material-ui/AutoComplete';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      candidates: []
+      candidates: [],
     };
   }
 
@@ -18,7 +19,6 @@ class App extends Component {
         return response.json();
       })
       .then((candidates) => {
-        console.log(candidates);
         candidates.sort(function(a,b){
           if (a.grade > b.grade) {
             return 1;
@@ -28,9 +28,29 @@ class App extends Component {
           }
           return 0
         })
-        return this.setState({candidates: candidates});
-      })
+      return this.setState({
+        candidates: candidates,
+      });
+    })
   }
+
+  handleUpdateInput = (value, candidates) => {
+    if (value) {
+        let matchedCandidates = [];
+        let searchCandidates = this.state.candidates;
+        value = value.charAt(0).toUpperCase() + value.slice(1);
+        for (var i = 0; i < searchCandidates.length; i++) {
+          if (searchCandidates[i].first_name.includes(value) || searchCandidates[i].last_name.includes(value)) {
+            matchedCandidates.push(searchCandidates[i])
+          }
+        this.setState({
+          candidates: matchedCandidates
+        })
+      }
+    } else {
+      this.componentDidMount()
+    }
+  };
 
   render() {
     if (this.state.candidates !== 0) {
@@ -39,7 +59,15 @@ class App extends Component {
           <header className="App-header">
             <img src={logo} className="App-logo" alt="Restless Bandit logo" />
           </header>
-          <main>
+          <form className="Section-form">
+            <AutoComplete
+              className="Searchbar"
+              hintText={<span><i className="material-icons">search</i> Search candidates...</span>}
+              dataSource={this.state.candidates}
+              onUpdateInput={this.handleUpdateInput}
+            />
+          </form>
+          <main className="Section-main">
             <Candidates candidates={this.state.candidates} />
           </main>
         </div>
--- src/Candidates.js
+++ src/Candidates.js
@@ -1,64 +1,62 @@
 import React from 'react';
-import { GridList, GridTile } from 'material-ui/GridList';
-import { Card, CardHeader, CardTitle, CardText } from 'material-ui/Card';
+import { GridList } from 'material-ui/GridList';
+import { Card, CardHeader, CardTitle } from 'material-ui/Card';
 import Avatar from 'material-ui/Avatar';
-
-import {
-  blue300,
-  indigo900,
-  orange200,
-  deepOrange300,
-  pink400,
-  purple500,
-} from 'material-ui/styles/colors';
+import { deepOrange300, purple500, indigo900, pink800 } from 'material-ui/styles/colors';
 
 
-const Candidates = ({ candidates }) => (
-  <GridList
-    cols={4}
-  >
-    {candidates.map((candidate, index) => (
-    <Card key={index}>
-    <Avatar
-          color={deepOrange300}
-          backgroundColor={purple500}
-          size={30}
-        >
-          {candidate.grade}
-        </Avatar>
-    <CardHeader
-      title={candidate.first_name + ' ' + candidate.last_name}
-    />
-    <CardTitle
-      title={candidate.last_position}
-      subtitle={candidate.last_company}
-    />
-    </Card>
-  ))}
-  </GridList>
-);
+const Candidates = ({ candidates }) => {
+  const style = {
+    cardHeader: {
+      fontSize: 25,
+      marginTop: -56,
+      color: indigo900
+    },
+    cardTitle: {
+      color: pink800,
+      fontSize: 15,
 
+    },
+    cardSubtitle: {
 
-export default Candidates;
+    }
+  };
 
-
-/**
-
-
-candidates.map((candidate, index) => (
-    <Card
-      key={index}
+  return (
+    <GridList
+      cols={4}
+      className="Grid-container"
     >
+      {candidates.map((candidate, index) => (
+      <Card
+        key={index}
+        className="Card"
+      >
+      <Avatar
+        color={deepOrange300}
+        backgroundColor={purple500}
+        size={30}
+        className="Avatar"
+      >
+        {candidate.grade}
+      </Avatar>
       <CardHeader
         title={candidate.first_name + ' ' + candidate.last_name}
+        className="Card-header"
+        titleStyle={style.cardHeader}
       />
       <CardTitle
         title={candidate.last_position}
-      />
-      <CardText>
-        {candidate.last_company}
-      </CardText>
-    </Card>
-    ))
+        subtitle={candidate.last_company}
+        className="Card-title"
+        titleStyle={style.cardTitle}
+        subtitleStyle={style.cardSubtitle}
 
- **/
+      />
+      </Card>
+    ))}
+    </GridList>
+  );
+};
+
+export default Candidates;
